21e271ac616aa04e939a17c268635012aa11f5b0
suwei8 <suwei8@outlook.com>
2025-06-03 22:01:39 +0800

    Update 3D-Tongji.py

--- 3D-Tongji.py
+++ 3D-Tongji.py
@@ -90,7 +90,8 @@ if __name__ == "__main__":
 
         to_users = [
             "oXUv66MibUi7VInLBf7AHqMIY438",
-            "oXUv66DvDIoQG39Vnspwj97QVLn4"
+            "oXUv66DvDIoQG39Vnspwj97QVLn4",
+            "oXUv66HUVNyZ0Hd8RWKmkVV1dkAs"
         ]
 
         common_data = {
